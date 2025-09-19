f32da6717760d6a985d8f42c8dbad1b8ec96f5d5
kangbaekho <kbh081004@gmail.com>
2025-09-19 13:52:01 +0900

    fix: ERC-721 폴더 구조 수정

--- ERC-721/contracts/erc721.sol
+++ ERC-721/contracts/erc721.sol
@@ -6,7 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract Erc721 is ERC721 {
     constructor() ERC721("baekho", "KBH") {}
 
+    function mint(address to, uint256 id) external {
+        _mint(to, id);
+    }
+
     function _baseURI() internal pure override returns (string memory) {
-        return "";
+        return "https://raw.githubusercontent.com/kangbaek324/ERCToken-study/main/ERC-721/metadata/";
     }
 }
